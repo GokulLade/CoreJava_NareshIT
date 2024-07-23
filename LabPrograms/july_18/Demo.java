b08688f530b7afeedb8bf9386b0350e38433b835
Gokul <gokullade89@gmail.com>
2024-07-23 23:22:52 +0530

    Pattern Program in java

--- LabPrograms/july_18/Demo.java
+++ LabPrograms/july_18/Demo.java
@@ -1,4 +1,3 @@
-package LabPrograms.july_18;
 import java.util.*;
 
 public class Demo {
@@ -6,10 +5,10 @@ public class Demo {
 	public static void main(String[] args) {
 		try (Scanner sc = new Scanner(System.in)) {
 			System.out.println("Enter Two Number");
-			int num1=sc.nextInt();
-			int num2=sc.nextInt();
-			
-			System.out.println("The Addition of "+num1+" and "+num2+" is :"+(num1+num2));
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+
+			System.out.println("The Addition of " + num1 + " and " + num2 + " is :" + (num1 + num2));
 		}
 
 	}
