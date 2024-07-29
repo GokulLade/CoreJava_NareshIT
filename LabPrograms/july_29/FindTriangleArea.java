94e7b6a36e294fccb1de090fcf52ed00d69a85bc
Gokul <gokullade89@gmail.com>
2024-07-29 17:17:43 +0530

    Finding Area for triangle using Method

--- LabPrograms/july_29/FindTriangleArea.java
+++ LabPrograms/july_29/FindTriangleArea.java
@@ -22,9 +22,11 @@ class FindTriangleArea
 {
 	static void findTriangleArea()
 	{
-		double side1=10.00,side2=15.00,side3=20.00,area;
+		double side1=10.00,side2=15.00,side3=20.00,area=0,s=0;
 		
-		area=(side1*side2*side3)/2.0;
+		s=(side1+side2+side3)/2;
+		
+		area=Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
 		
 		System.out.println(area);
 	}
